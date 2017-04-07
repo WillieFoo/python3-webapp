6203a2537551f4245c6a39117c653892f1067d59
WillieFoo <fuweiping@babybus.com>
2017-04-07 16:27:37 +0800

    这是test分支上的测试

--- www/app.py
+++ www/app.py
@@ -36,3 +36,11 @@ async def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+
+# I'm writting a letter now.
+# Dear P.T.:
+#   ...
+#   ...
+#   ...
+#   see you!
